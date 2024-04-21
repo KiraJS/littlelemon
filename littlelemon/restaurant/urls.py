7f6fd06543bf63ba85d76a901756a2a73a0c845d
Kira Shatulova <kira.shatulova@iceye.fi>
2024-04-21 19:26:16 +0300

    add menu crud

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import index 
+from .views import index, MenuItemsView, SingleMenuItemView
   
 urlpatterns = [ 
     path('', index, name='index'), 
+    path('menu-items/', MenuItemsView.as_view(), name='menu-items-list'),
+    path('menu-items/<int:pk>/', SingleMenuItemView.as_view(), name='single-menu-item'),
+
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
